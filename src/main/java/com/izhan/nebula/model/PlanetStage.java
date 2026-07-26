6e93360a0e4d8db099560915a2fec6ad9a73d730
isabelleezhan <isabellehzhan@gmail.com>
2026-07-26 02:55:20 -0700

    Add PlanetStage enum

---
+++ src/main/java/com/izhan/nebula/model/PlanetStage.java
@@ -0,0 +1,11 @@
+package com.izhan.nebula.model;
+
+// Represents the major visual stages in a planet's evolution.
+public enum PlanetStage {
+    BARREN,
+    ATMOSPHERE,
+    TERRAIN,
+    BIOSPHERE,
+    CIVILIZATION,
+    COMPLETE
+}
